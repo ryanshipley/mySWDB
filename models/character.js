2ef541b70bc076d368c297f6c3e73891dc25033a
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-03 11:00:12 -0400

    adds initial route

--- models/character.js
+++ models/character.js
@@ -1,1 +1,23 @@
-module.exports = [];
+module.exports = [
+    {
+        name : "Luke Skywalker",
+        species : "human",
+        height: "1.72m",
+        planet : "Tatooine",
+        birthworld : "Polis Massa",
+        appearances : [
+            "Episode 3",
+            "Episode 4",
+            "Episode 5", 
+            "Episode 6",
+            "Episode 7",
+            "Episode 8",
+            "Episode 9",
+            "The Mandalorian",
+        ],
+        image : "https://i.imgur.com/akzHJEl.png",
+        starships : "X-wing",
+        weapon : "Lightsaber (green)",
+        associations : ["Jedi Order" , "Rebel"]
+    }
+];
--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require("express");
 const app = express();
+
+app.get("/" , (req, res)=>{
+    res.send("Welcome");
+});
+
+app.listen(3000);
